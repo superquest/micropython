0247eac721e790d10b21fb199a4e3a90acebfc24
Justin Moen <jamoen7@gmail.com>
2018-09-07 21:02:19 -0500

    plain-text seed IO

--- ports/unix/src/keys.py
+++ ports/unix/src/keys.py
@@ -1,34 +1,25 @@
 import urandom
 import tcc
 
-def entropy(n: int):
-    return urandom.getrandbits(n)
 
+SEED_FILE = "seed.txt"
+
+def entropy(n: int):
+    # TODO: hardware RNG
+    return bytes([urandom.getrandbits(8) for _ in range(n)])
 
 def double_sha256(message: bytes) -> bytes:
     return tcc.sha256(tcc.sha256(message).digest()).digest()
 
-
-def read_seed():
-    # this would come from disk
-    return b'\x98Y\xcc0\xc5\xcd%\x81\xda\x950\x8a~\xe3\xc3l\xf0\xd2KA0\x1f\xdei\xd1\xa4j\x95\xc6\x1cW\x8d\x80\xbfc(|\xba\xde\xfa\x1a\xfc\xc2d7\t9\xaa_#\xe6|j\x18\xa2S\x8b\x97=\xd2\xe9Z\x13k'
-
-
-def derive_node(path: list, curve_name: str = "secp256k1") -> tcc.bip32.HDNode:
-    seed = read_seed()
-    old = node = tcc.bip32.from_seed(seed, curve_name)
+def derive_node(seed, path: list, curve_name: str = "secp256k1") -> tcc.bip32.HDNode:
+    print(seed)
+    node = tcc.bip32.from_seed(seed, curve_name)
     node.derive_path(path)
     return node
-
 
 def sign_message(node: tcc.bip32.HDNode, message: bytes) -> bytes:
     private_key = node.private_key()
     message_digest = double_sha256(message)
-
-    print("SIGNING...")
-    print("private_key=", private_key)
-    print("message_digest=", message_digest)
-
     sig = tcc.secp256k1.sign(private_key, message_digest)
     return sig
 
@@ -37,3 +28,18 @@ def verify(node: tcc.bip32.HDNode, signature: bytes, message: bytes) -> bool:
     message_digest = double_sha256(message)
     return tcc.secp256k1.verify(public_key, signature, message_digest)
 
+def save_seed_to_disk(seed):
+    open(SEED_FILE, "wb").write(seed)
+
+def init_device():
+    data = entropy(32)
+    print("bip39 entropy=", data)
+    mnemonic = tcc.bip39.from_data(data)
+    print("mnemonic=", mnemonic)
+    passphrase = ""
+    seed = tcc.bip39.seed(mnemonic, passphrase)
+    print("seed=", seed)
+    save_seed_to_disk(seed)
+
+def load_seed_from_disk():
+    return open(SEED_FILE, 'rb').read()
--- ports/unix/src/main.py
+++ ports/unix/src/main.py
@@ -1,14 +1,29 @@
 import tcc
 import keys
 
-def main():
+
+def sign_and_verify(seed):
     path = [0, 0, 0, 0]
     message = b"test"
-
-    node = keys.derive_node(path)
+    node = keys.derive_node(seed, path)
+    print("signing...")
     sig = keys.sign_message(node, message)
-
     print("verifies?", keys.verify(node, sig, message))
+
+def init():
+    keys.init_device()
+    print("saved seed to disk")
+
+def boot():
+    seed = keys.load_seed_from_disk()
+    print("read seed from disk")
+    return seed
+
+def main():
+    init()
+    seed = boot()
+    sign_and_verify(seed)
+
 
 if __name__ == '__main__':
     main()
